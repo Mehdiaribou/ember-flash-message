453531d8ade36af923599dbef627d62ec2e90800
Mehdi <laribou@my.com>
2024-05-14 15:00:32 +0100

    ember-flash-message

---
+++ README.md
@@ -0,0 +1,117 @@
+# Ember Flash Message
+
+A simple plugin that allows you to display a message on the next route
+transition, similiar to Rails' ``flash[:notice]``. This is useful for
+displaying one time notices on later pages.
+
+## Usage
+
+You should include
+[ember-flash-message.js](https://raw.github.com/ryanto/ember-flash-message/master/flash-message.js)
+on your page. Since the plugin depends on Ember.js you will have to
+include Ember first. Now any of your route's will have access to
+``flashMessage('message')``, which will set the message.
+
+### Template
+
+Any template that you want to display the message should contain the
+following. It makes the most sense to put this in your ``application``
+template to have the message be available on all pages.
+
+```handlebars
+{{#flashMessage}}
+  <div {{bind-attr class="message.type"}}>
+    {{message.text}}
+  </div>
+{{/flashMessage}}
+```
+
+### Route
+
+Any route can call ``this.flashMessage(message)`` to set the message.
+The message will be displayed after the next transition. After the
+router transitions for a second time the message will be destroyed.
+
+```javascript
+Ember.LoginRoute = Ember.Route.extend({
+  actions: {
+    login: function() {
+      // login user ...
+      this.flashMessage('Welcome back!', 'success');
+      this.transitionTo('index');
+    }
+  }
+});
+```
+
+### Instant Message
+
+There may be some instances where you want to display the message right
+away and not wait for the route to transition. You can use the ``now()``
+function to update the message.
+
+```javascript
+Ember.ProfileRoute = Ember.Route.extend({
+  actions: {
+    save: function(profile) {
+      var router = this;
+
+      profile.save().then(function() {
+        router.flashMessage('Your profile has been updated!').now();
+      });
+    }
+  }
+});
+```   
+
+### Dismissing Messages
+
+While you could easily remove flash messages yourself, the main FlashMessageController contains a convinence [action](http://emberjs.com/guides/templates/actions/) named `dismissFlashMessage` that will remove the current message. To use this action, you just need to add a link _(or any other element)_ that triggers it.
+
+```handlebars
+{{#flashMessage}}
+  <div {{bind-attr class=':flash-message message.type'}}>
+    {{message.text}}
+    <a href='#' class='dismiss' {{action 'dismissFlasshMessage'}}>x</a>
+  </div>
+{{/flashMessage}}
+```
+
+### Controller
+
+The flash message can be set from the controller by adding a ``needs``
+dependency for the ``flashMessage`` controller. See the example below.
+
+
+```javascript
+App.PostController = Ember.ObjectController.extend({
+  needs: ['flashMessage'],
+
+  actions: {
+    save: function() {
+      var flashMessage = this.get('controllers.flashMessage');
+
+      this.get('model').save()
+        .then(function() {
+          flashMessage.set('message', 'Blog post saved!');
+        });
+    }
+  }
+});
+```
+
+Please note that whenever you set the flash message from a control it
+will be displayed instantly.
+
+## Development
+
+This plugin is built with rake pipeline, which requires Ruby. To get
+started:
+
+```
+bundle install
+bundle exec rackup
+```
+
+Edit code and visit [http://localhost:9292](http://localhost:9292) to
+run tests.
---
+++ tests/integration/flash_message_test.js
@@ -0,0 +1,213 @@
+var App = Ember.Application.create({
+  rootElement: '#qunit-fixture'
+});
+
+App.Router.map(function() {
+  this.route('page1');
+  this.route('page2');
+  this.route('promise');
+  this.resource('posts', { path: '/posts' }, function() {
+    this.route('new');
+  });
+  this.route('fromController');
+});
+
+App.PromiseRoute = Ember.Route.extend({
+  model: function() {
+    return new Ember.RSVP.Promise(function(resolve) {
+      Ember.run.later(function() {
+        resolve();
+      }, 100);
+    });
+  }
+});
+
+App.LoadingRoute = Ember.Route.extend();
+
+App.FromControllerController = Ember.Controller.extend({
+  needs: 'flashMessage'.w(),
+
+  actions: {
+    showMessage: function() {
+      var flashMessage = this.get('controllers.flashMessage');
+      flashMessage.set('message', 'testing');
+    }
+  }
+});
+
+Ember.TEMPLATES.application = Ember.Handlebars.compile('{{#flashMessage}}<span {{bind-attr class=":message message.type"}}>{{message.text}}</span>{{/flashMessage}}');
+
+var findMessage = function() {
+  return $('#qunit-fixture .message');
+};
+
+var hasClass = function(className) {
+  return $('#qunit-fixture .message').hasClass(className);
+};
+
+var messageExists = function() {
+  return findMessage().length > 0;
+};
+
+var assertMessage = function() {
+  return ok(messageExists());
+};
+
+var assertNoMessage = function() {
+  return ok(!messageExists());
+};
+
+var router = function() {
+  return App.__container__.lookup('route:application');
+};
+
+App.setupForTesting();
+
+module("Integration App Test", {
+  setup: function() {
+    App.reset();
+    App.injectTestHelpers();
+  }
+});
+
+test("should not see the flash if there is no message", function() {
+  visit("/");
+  andThen(assertNoMessage);
+});
+
+test("should not see the flash message if there has not been a transition", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('hello world');
+  });
+
+  andThen(assertNoMessage);
+});
+
+test("should see a flash message when I transition to the next route", function() {
+  expect(2);
+
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('hello world');
+  });
+
+  visit("/page1");
+
+  andThen(assertMessage);
+
+  andThen(function() {
+    equal(findMessage().text().trim(), 'hello world');
+  });
+});
+
+test("should have a message object with text and type defined", function() {
+  expect(3);
+
+  visit('/');
+
+  andThen(function() {
+    router().flashMessage('Happy Message', 'success');
+  });
+
+  visit("/page1");
+
+  andThen(assertMessage);
+
+  andThen(function() {
+    equal(findMessage().text().trim(), 'Happy Message');
+    ok(hasClass('success'));
+  });
+});
+
+test("should not see a flash message once it has been displayed", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('test');
+  });
+
+  visit("/page1");
+
+  andThen(assertMessage);
+
+  visit("/page2");
+
+  andThen(assertNoMessage);
+});
+
+test("should not display or destroy the flash message when in the loading route", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('test');
+  });
+
+  visit("/promise");
+
+  andThen(assertMessage);
+});
+
+test("should have an action that dismisses the current flash message", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('Dismiss Me').now();
+  });
+
+  andThen(function() {
+    equal(findMessage().text().trim(), 'Dismiss Me');
+    App.__container__.lookup('controller:flashMessage')
+      .send('dismissFlashMessage');
+    assertNoMessage();
+  });
+
+});
+
+test("should display the flash message instantly", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('instant message').now();
+  });
+
+  andThen(assertMessage);
+});
+
+test("should display the flash message for resource", function() {
+  visit("/");
+
+  andThen(function() {
+    router().flashMessage('test');
+  });
+
+  visit("/posts/new");
+
+  andThen(assertMessage);
+
+});
+
+test("should be able to use the flash messenger from a controller", function() {
+  visit("/");
+
+  andThen(function() {
+    assertNoMessage();
+  });
+
+  visit("/fromController");
+
+  andThen(function() {
+    assertNoMessage();
+  });
+
+  andThen(function() {
+    App.__container__.lookup('controller:fromController')
+      .send('showMessage');
+  });
+
+  andThen(function() {
+    assertMessage();
+  });
+});
